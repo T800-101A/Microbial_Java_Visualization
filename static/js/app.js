375e46ff75b59c2a112cd9a1c3ddbdcb53d5d441
vincent@t-101.com <vincent@t-101.com>
2023-10-06 01:30:04 -0500

    Code updated
    finishing code with problems fixed

--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,31 @@
+
 ////////////////////////////////////////////////////////////
-
-
 function init() {
   var selector = d3.select("#selDataset");
   
   // Dropdown with subject ID's from the list of sample Names
   d3.json("../samples.json").then((data) => {
     var subjectIds = data.names;
-      subjectIds.forEach((id) => {
-        selector
-        .append("option")
-        .text(id)
-        .property("value", id);
-      });
+
+
+    for(let i = 0; i < subjectIds.length; i++) {
+      selector
+      .append("option")
+      .text(subjectIds[i])
+      .property("value", subjectIds[i]);
+    };
     
-    // Use the first subject ID from the names to build initial plots
-    const firstSubject = subjectIds[0];
+    
+    const firstSubject = subjectIds[0];// First subjectIDs from the names to build initial plots
     updateCharts(firstSubject); //Calling update funtion for Charts
     updateMetadata(firstSubject); //Calling Function to Update Metadata
+    
   });
 };
 
-////////////////////////////////////////////////////////////
 
 
-
-
-function updateMetadata(sample) {
-  d3.json("../samples.json").then((data) => {
-
-    var metadata = data.metadata;
-    var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
-    var result = filterArray[0];
-    var metaPanel = d3.select("#sample-metadata");
-    metaPanel.html("");
-    
-    Object.entries(result).forEach(([key, value]) => {
-      metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
-    })
-  
-              ///////////
-    
-
-  });
-}
+//////////////////////////////Creating Function for Plots////////////////////////////////////////
 
 
 function updateCharts(sample) {    
@@ -59,8 +41,28 @@ function updateCharts(sample) {
   var otu_labels = result.otu_labels; 
   
   
+  //////////////////////////////Defining/Creating Bar Plot////////////////////////////////////////
+      
+  var trace1 = { 
+    x: sample_values.slice(0,10).reverse(),
+    y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+    text: otu_labels.slice(0,10).reverse(),
+    name: "PLLT",
+    type: "bar",
+    orientation: "h"
+  };
+  var data = [trace1];
+  var layout = {
+    title: "Top Ten OTUs for Individual " +sample,
+    margin: {l: 150, r: 50, t: 50, b: 50}
+  };
 
-  // Defining val for bubble Chart
+  Plotly.newPlot("bar", data, layout);
+
+
+  /////////////////////////////Defining/Creating val for bubble Plot/////////////////////////////////////////
+
+  
   var trace1 = {
       x: otu_ids,
       y: sample_values,
@@ -69,53 +71,56 @@ function updateCharts(sample) {
       marker: {
       size: sample_values,
       color: otu_ids,
-      colorscale:"Electric"
-    }
+      colorscale:"Ocean"
+      }
   };
-  
+
   var data = [trace1];
-
-
-
   var layout = {
-      title: 'Bacteria Cultures per Sample',
+      title: 'Bacteria Cultures / Sample',
       showlegend: false,
       hovermode: 'closest',
-      xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
-      margin: {t:30}
-  
-
-
-    };Plotly.newPlot('bubble', data, layout); 
-    
-    
-    
-    // Defining val. for bar Chart
-    
-    var trace1 = {
-      x: sample_values.slice(0,10).reverse(),
-      y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
-      text: otu_labels.slice(0,10).reverse(),
-      name: "Greek",
-      type: "bar",
-      orientation: "h"
-    };
-    var data = [trace1];
-    var layout = {
-      title: "Top Ten OTUs for Individual " +sample,
-      margin: {l: 100, r: 100, t: 100, b: 100}
-    };
-    Plotly.newPlot("bar", data, layout);
+      xaxis: {title:"OTU (Operational Taxonomic Unit) ID " + sample},
+      margin: {t:50}
+    };    
+    Plotly.newPlot('bubble', data, layout);
   });
-
-
 
 };
 
 
-function optionChanged(newSample) { // Update new data each time a sample is selected it
+
+
+//////////////////////////////Creating Function for Metadata ////////////////////////////////////////
+
+function updateMetadata(sample) {
+  d3.json("../samples.json").then((data) => {
+
+    var metadata = data.metadata;
+    var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
+    var result = filterArray[0];
+    var metaPanel = d3.select("#sample-metadata");
+    metaPanel.html("");
+    
+    var entries = Object.entries(result);
+    for(let i = 0; i < entries.length; i++) {
+      let key = entries[i][0];
+      let value = entries[i][1];
+      metaPanel.append("h5").text(`${key.toUpperCase()}: ${value}`);
+    }});
+  
+  };
+    
+
+
+ ///////////////////////////Update new data each time a sample is selected it/////////////////////////////////////////
+
+
+ function optionChanged(newSample) { // 
   updateCharts(newSample);
   updateMetadata(newSample);
-}
+};
+
+
 
 init();
